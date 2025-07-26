6bf39c72b905e96505914a7052c9e357793f00c6
Sudarma Yudho Prayitno <sudarmayudhoprayitno22@gmail.com>
2025-07-26 11:22:33 +0700

    Update diagnosisRoutes.js

--- backend/src/routes/diagnosisRoutes.js
+++ backend/src/routes/diagnosisRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { submitDiagnosis } from '../controllers/diagnosisController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
-const { authenticateToken } = require('../middleware/auth');
+// const { authenticateToken } = require('../middleware/auth');
 
 const router = express.Router();
 
 // Rute ini dilindungi oleh autentikasi
-router.post('/submit', authenticateToken, diagnosisController.submitDiagnosis);
+router.post('/submit', authMiddleware, submitDiagnosis);
 
 export default router;
